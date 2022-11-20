65f1dfbbe3bed23494a9c9ace3c6158c81b2b905
Daniel Makover <DanielM@centiro.se>
2022-11-20 09:56:58 -0500

    page controller- added mobile

--- controllers/page.controller.js
+++ controllers/page.controller.js
@@ -10,16 +10,25 @@ export default class PageController{
         res.render('VerticalMario',{loggedIn: x, mobile});
     }
     static async getLoginPage(req, res){
+        var source = req.headers['user-agent'];
+        var ua = ExpressUserAgent.parse(source);
+        var mobile = ua.isMobile
         var x = loggedIn(req, res);
-        res.render('login', {loggedIn: x});
+        res.render('login', {loggedIn: x, mobile});
     }
 
     static async getAdminPage(req, res ){
+        var source = req.headers['user-agent'];
+        var ua = ExpressUserAgent.parse(source);
+        var mobile = ua.isMobile
         var x = loggedIn(req, res);
-        res.render('admin',{loggedIn: x});
+        res.render('admin',{loggedIn: x, mobile});
     }
     static async getRegistrationPage(req, res){
+        var source = req.headers['user-agent'];
+        var ua = ExpressUserAgent.parse(source);
+        var mobile = ua.isMobile
         var x = loggedIn(req, res);
-        res.render('register',  {loggedIn: x});
+        res.render('register',  {loggedIn: x, mobile});
     }
 }
